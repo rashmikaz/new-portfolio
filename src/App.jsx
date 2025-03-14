6d7054574f392dfc7e19dececf36fe50e0753d7e
rashmikaz <phrashmika@gmail.com>
2025-03-14 16:46:11 +0530

    boarder

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,9 @@ const App = () => {
   return (
     <div>
       <Navbar />
+      <div className="max-w-7xl mx-auto pt-5 px-6">
+
+      </div>
       <Mainsection />
     </div>
   )
--- src/components/Mainsection.jsx
+++ src/components/Mainsection.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import mainimg from "../assets/new main.png"
 
 
 
@@ -8,19 +9,23 @@ const Mainsection = () => {
 
 
      <div className="w-100 relative right-110 top-30">
-     <h1 className="text-4xl sm:text-6xl lg:text-8xl tracking-wide">
-        Desinger
+     <h1 className="font-bold text-4xl sm:text-6xl lg:text-8xl tracking-wide">
+        desinger
       </h1>
-      <p>UI-focused product designer specializing in design systems and user interface design.</p>
+      <p className="text-lg text-center text-neutral-500 max-w-4xl">UI-focused product designer specializing in design systems and user interface design.</p>
      </div>
 
      <div className="w-100 relative left-90 bottom-5">
-     <h1 className="text-4xl sm:text-6xl lg:text-8xl tracking-wide">
-        Developer
+     <h1 className="font-bold text-4xl sm:text-6xl lg:text-8xl tracking-wide">
+        developer
       </h1>
-      <p>Frontend and Backend developer who write clean,elegant and efficient code.</p>
+      <p className="text-lg text-center text-neutral-500 max-w-4xl">Frontend and Backend developer who write clean,elegant and efficient code.</p>
      </div>
 
+     <div className="relative bottom-80">
+        <img src={mainimg} alt="" />
+     </div>
+     <div className="border-t border-gray-300 w-full mt-10 relative bottom-90"></div>
     </div>
   )
 }
