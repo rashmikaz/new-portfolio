8f36b804e7e47feb70709bf4f185e2b64e3c76db
rashmikaz <phrashmika@gmail.com>
2025-03-15 03:24:22 +0530

    about section

--- src/components/Aboutsection.jsx
+++ src/components/Aboutsection.jsx
@@ -2,13 +2,11 @@ import React from 'react';
 
 const Aboutsection = () => {
   return (
-    <div className="py-16 px-4 bg-gray-100">
-      <div className="container mx-auto text-center">
-        <h2 className="text-4xl font-semibold mb-4">About Me</h2>
-        <p className="text-lg text-neutral-500 max-w-3xl mx-auto mb-8">
-          I'm Rashmika, a passionate UI/UX designer and software engineering student. I specialize in crafting seamless digital experiences with clean and user-friendly interfaces.
-        </p>
-      </div>
+    <div className="flex flex-col items-center mt-6 lg:mt-24">
+     <div>
+      <h1>about</h1>
+      <p>I'm a  designer & developer  in Sri Lanka, with a passion for creating intuitive and user-friendly digital experiences</p>
+     </div>
     </div>
   );
 };
