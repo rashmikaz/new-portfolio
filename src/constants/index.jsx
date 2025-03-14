c1d314b93dc1712de90b5d97a0a5f00825e92a41
rashmikaz <phrashmika@gmail.com>
2025-03-14 10:00:39 +0530

    nav items on constants

---
+++ src/constants/index.jsx
@@ -0,0 +1,10 @@
+
+
+export const navItems = [
+    { label: "Project", href: "#" },
+    { label: "Services", href: "#" },
+    { label: "About", href: "#" },
+    { label: "Testimonials", href: "#" },
+    { label: "Contact", href: "#" },
+  ];
+  
