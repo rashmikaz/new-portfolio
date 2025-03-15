981dbea8f12269af92e65127502d4f1c5da908e5
rashmikaz <phrashmika@gmail.com>
2025-03-15 12:18:13 +0530

    contact section heading

---
+++ src/components/Contact.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <div className="flex flex-col items-center mt-6 lg:mt-24">
+        <div>
+            <h1  className="font-bold text-4xl sm:text-6xl lg:text-8xl tracking-wide">contact</h1>
+            <p className="text-xl  text-neutral-500 pt-6 pb-6">Get in touch with me via social media or send me an email</p>
+        </div>
+      
+    </div>
+  )
+}
+
+export default Contact
