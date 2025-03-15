ee212053a7893b3bc65f7d03b121edb0bba4ccb3
rashmikaz <phrashmika@gmail.com>
2025-03-15 13:35:33 +0530

    skills

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import aboutimg from "../assets/main.png"
+import Skill from './Skills';
 
 const Aboutsection = () => {
   return (
@@ -9,7 +10,7 @@ const Aboutsection = () => {
      
       <p className="text-xl  text-neutral-500 pt-6 pb-6">I'm a  designer & developer  in Sri Lanka, with a passion for creating intuitive and user-friendly digital experiences</p>
      
-      <p className="text-xl  text-neutral-100">I’m Rashmika, a passionate and dedicated software engineering student with a deep interest in UI/UX design. My journey in the world of technology started with a curiosity for problem-solving and a fascination with how digital tools can improve user experiences.
+      <p className="text-xl text-neutral-100 leading-8">I’m Rashmika, a passionate and dedicated software engineering student with a deep interest in UI/UX design. My journey in the world of technology started with a curiosity for problem-solving and a fascination with how digital tools can improve user experiences.
       I have honed my skills through hands-on learning and projects that focus on creating intuitive, user-friendly interfaces.My work emphasizes clean code, thoughtful design, and a focus on how users interact with technology, ensuring every project is not only functional but delightful to use.
       Alongside my academic pursuits
       </p>
@@ -17,6 +18,10 @@ const Aboutsection = () => {
 
      <div className="relative bottom-130 left-70">
       <img src={aboutimg} alt="" />
+     </div>
+
+     <div>
+      <Skill />
      </div>
 
     </div>
