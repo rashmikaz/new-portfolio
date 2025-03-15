4f6f2ad55d4cc2f4159c873ac9fb3d2bf062689d
rashmikaz <phrashmika@gmail.com>
2025-03-16 01:10:35 +0530

    all card done in the main

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,6 +2,7 @@ import React, { useRef } from 'react';
 import { Link } from 'react-router-dom';
 import mainimg from "../assets/main.png";
 import portfolioimg from "../assets/portfolio.jpeg";
+import innovestaimg from "../assets/innovesta.jpeg"
 import Footer from './Footer';
 import { Card } from "flowbite-react";
 
@@ -109,17 +110,37 @@ const Main = () => {
       <p className="font-normal text-gray-700 dark:text-gray-400">
         Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.
       </p>
-    </Card>
+      </Card>
 
-    <Card className="max-w-sm relative bottom-130" imgSrc={portfolioimg} horizontal>
-      <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+      <Card
+      className="max-w-sm relative bottom-215 "
+      imgAlt="Meaningful alt text for an image that is not purely decorative"
+      imgSrc={innovestaimg}
+    >
+      <h5 className="text-2xl font-bold tracking-tight text-gray-900 ">
+        Noteworthy technology acquisitions 2021
+      </h5>
+      <p className="font-normal text-gray-700 dark:text-gray-400">
+        Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.
+        Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.
+        Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.
+      </p>
+      </Card>
+
+      <Card
+      className="max-w-sm relative bottom-385 left-100"
+      imgAlt="Meaningful alt text for an image that is not purely decorative"
+      imgSrc={portfolioimg}
+    >
+      <h5 className="text-2xl font-bold tracking-tight text-gray-900 ">
         Noteworthy technology acquisitions 2021
       </h5>
       <p className="font-normal text-gray-700 dark:text-gray-400">
         Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.
       </p>
-    </Card>
-    
+      </Card>
+
+   
 
      <div className="relative bottom-110">
      <Footer />
