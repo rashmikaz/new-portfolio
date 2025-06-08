4ee76e9ab3c23b09015a3c5bc029934ec9bd6105
rashmikaz <phrashmika@gmail.com>
2025-06-08 09:48:58 +0530

    add logo for nav bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,8 +7,9 @@ const Navbar = () => {
     <nav className="sticky top-0 z-50 py-3 backdrop-blur-lg border-b border-neutral-700/80">
       <div className="container px-3.7 mx-auto relative lg:text-sm">
         <div className="flex justify-between items-center">
-          <div className="flex items-center flex-shrink-0 relative left-25">
-            <span className="text-xl tracking-tight">RK</span>
+          <div className="flex items-center flex-shrink-0 relative left-25 w-16 h-16">
+            {/* <span className="text-xl tracking-tight">RK</span> */}
+            <img src="./logo.png" alt="" />
           </div>
           <ul className="hidden lg:flex ml-14 space-x-12">
             {navItems.map((item, index) => (
